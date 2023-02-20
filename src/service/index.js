1392e8753836c28d670f0b77ca741e56cb71694f
AgistStudio <junior.ho@gmail.com>
2023-02-20 16:49:59 -0400

    Update index.js

--- src/service/index.js
+++ src/service/index.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 const defaultAxios = Axios.create({
   headers: {
     Authorization:
-      "Bearer Enter lemon.markets market data API token here",
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJsZW1vbi5tYXJrZXRzIiwiaXNzIjoibGVtb24ubWFya2V0cyIsInN1YiI6InVzcl9yeURYWWxsRERUbHJLUTdtNE5ENVltUjBobmdyd1Zmck5DIiwiZXhwIjoxNzA4Mjg0NTk0LCJpYXQiOjE2NzY3NDg1OTQsImp0aSI6ImFwa19yeURYWWxsR0d5RmdXU2JXZldxVGtoczNkWVF6RGtkcUNOIiwibW9kZSI6InBhcGVyIn0.m5rem9XTx3P3PGo-8wN_G4x7Qe38qqbMGkn1ocxi32Y",
     "Content-Type": "application/json",
   },
 });
